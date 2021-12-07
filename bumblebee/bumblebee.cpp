ff74ad233ada0c181b78fcf26f49201e87938e50
tamirco2003 <tamir.cohen835@gmail.com>
2021-12-07 14:33:56 +0200

    Added user input.

--- bumblebee/bumblebee.cpp
+++ bumblebee/bumblebee.cpp
@@ -2,13 +2,19 @@
 #include <iostream>
 
 int main() {
-	const int PRIME_COUNT = 100;
-	int primeArray[PRIME_COUNT];
+	int primeCount = 0;
 
-	primeGenerator(PRIME_COUNT, primeArray);
-	for (int i = 0; i < PRIME_COUNT; i++) {
+	std::cout << "Number of primes to generate: ";
+	std::cin >> primeCount;
+
+	int* primeArray = new int[primeCount];
+
+	primeGenerator(primeCount, primeArray);
+	for (int i = 0; i < primeCount; i++) {
 		std::cout << primeArray[i] << std::endl;
 	}
+
+	delete[] primeArray;
 
 	return 0;
 }
